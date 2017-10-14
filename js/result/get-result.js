c7454a3b2f198f6e7fa0e6133591637859a29d96
platonovAlexey <kalyawe4ka@gmail.com>
2017-10-14 19:39:37 +0300

    fix get-result

--- js/result/get-result.js
+++ js/result/get-result.js
@@ -1,11 +1,14 @@
 const getResult = (statistics, result) => {
+  statistics = statistics.slice();
+
   if (result.timeLeft === 0) {
     return `Время вышло! Вы не успели отгадать все мелодии`;
   }
   if (result.attempts === 0) {
     return `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
   }
-  statistics.slice(result.score);
+
+  statistics.push(result.score);
   statistics.sort((left, right) => right - left);
   const amountOfPlayers = statistics.length;
   const position = statistics.lastIndexOf(result.score) + 1;
