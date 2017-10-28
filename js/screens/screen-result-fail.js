299325ff9fafcbecfd6f1c6a4a6982b04c074620
platonovAlexey <kalyawe4ka@gmail.com>
2017-10-28 20:53:51 +0300

    вернул обратно файлы

---
+++ js/screens/screen-result-fail.js
@@ -0,0 +1,21 @@
+import getElementFromTemplate from '../helpers/get-element-from-template';
+import renderScreen from '../helpers/show';
+import screenWelcome from './screen-welcom';
+
+const template = `<section class="main main--result">
+    <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
+
+    <h2 class="title">Какая жалость!</h2>
+    <div class="main-stat">У вас закончились все попытки.<br>Ничего, повезёт в следующий раз!</div>
+    <span role="button" tabindex="0" class="main-replay">Попробовать ещё раз</span>
+  </section>`;
+
+const screenResultFail = getElementFromTemplate(template);
+const replayButton = screenResultFail.querySelector(`.main-replay`);
+
+
+replayButton.addEventListener(`click`, () => {
+  renderScreen(screenWelcome);
+});
+
+export default screenResultFail;
