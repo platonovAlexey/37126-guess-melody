7d15663f68b61443db9725ad46f45738422309b0
platonovAlexey <kalyawe4ka@gmail.com>
2017-11-01 23:37:51 +0300

    stage state

--- js/application.js
+++ js/application.js
@@ -16,6 +16,9 @@ const routes = {
 };
 
 const loadGame = (data) => {
+  if (data) {
+    data = `{"lives": ${data.substr(0, 1)}, "level":${+data.substr(1, 2)}, "time": ${+data.substr(3, 3)}, "score": ${+data.substr(6)}}`;
+  }
   try {
     return JSON.parse(data);
   } catch (e) {
@@ -24,7 +27,15 @@ const loadGame = (data) => {
 };
 
 const saveGame = (game) => {
-  return JSON.stringify(game);
+  if (game) {
+    const stringLevel = game.level.toString().length === 2 ? game.level : `0${game.level}`;
+    const stringTime = game.time.toString().length === 3 ? game.time : `${new Array(3 - game.time.toString().length).fill(`0`).join(``)}${game.time}`;
+    const stringScore = game.score.toString().length === 2 ? game.score : `0${game.score}`;
+
+    return `${game.lives}${stringLevel}${stringTime}${stringScore}`;
+  } else {
+    return JSON.stringify(game);
+  }
 };
 
 export default class Application {
@@ -49,7 +60,7 @@ export default class Application {
     location.hash = ControllerSTATE.WELCOME;
   }
 
-  static startGame(game = makeState()) {
+  static changeLevel(game = makeState()) {
     routes[ControllerSTATE.GAME] = new GameScreen();
     location.hash = `${ControllerSTATE.GAME}?${saveGame(game)}`;
   }
--- js/game/game.js
+++ js/game/game.js
@@ -4,21 +4,21 @@ import LevelGenreView from './levels/level-gender-view';
 import GameModel from './game-model';
 import setScreen from '../helpers/set-screen';
 import gameOver from './game-over/game-over';
-import {levels, defaultState, Result} from '../data/data';
+import {levels, makeState, Result} from '../data/data';
 
 export default class GameScreen {
   constructor(data = levels) {
     this.model = new GameModel(data);
-    this.view = this.getLevelType(defaultState);
-
-    this.view.onAnswer = (answer) => this.onAnswer(answer);
+    this.view = this.getLevelType(makeState());
   }
 
-  init(game = defaultState) {
+  init(game = makeState()) {
     this.model.update(game);
-    this.model.setTimer();
+    this.model.setTimer(game.time);
 
     this.model.timer.start();
+    this.view = this.getLevelType(this.model.game);
+    this.view.onAnswer = (answer) => this.onAnswer(answer);
     gameOver();
 
     setScreen(this.view);
@@ -41,7 +41,7 @@ export default class GameScreen {
       case Result.DIE: {
         this.model.die();
         this.model.setGameTime();
-        this.changeLevel();
+        App.changeLevel(this.model.game);
         break;
       }
       case Result.FAIL: {
@@ -51,7 +51,6 @@ export default class GameScreen {
       case Result.WIN: {
         const startLevelTime = this.model.game.time;
         this.model.setGameTime();
-
         this.model.setStat(startLevelTime - this.model.getAnswerTime());
         this.model.getGamePoints();
 
@@ -65,19 +64,9 @@ export default class GameScreen {
         this.model.setStat(startLevelTime - this.model.getAnswerTime());
 
         this.model.nextLevel();
-        this.changeLevel();
+        App.changeLevel(this.model.game);
         break;
       }
     }
-  }
-
-  changeLevel() {
-    this.view = this.getLevelType(this.model.game);
-
-    this.model.timer.start();
-
-    this.view.onAnswer = (answer) => this.onAnswer(answer);
-
-    setScreen(this.view);
   }
 }
