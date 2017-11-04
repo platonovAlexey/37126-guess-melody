d41c260df9797abaa45bcba5385e34821e4f312d
platonovAlexey <kalyawe4ka@gmail.com>
2017-11-04 18:57:49 +0300

    не там начал удалять файлы

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,24 @@
-import {SERVER_URL} from './data/data';
+import {SERVER_URL, DEFAULT_USER} from './data/data';
 
 export default class Loader {
   static load() {
     return fetch(`${SERVER_URL}`).then((response) => response.json());
   }
+
+  static loadResult(userName = DEFAULT_USER) {
+    return fetch(`${SERVER_URL}/stats/${userName}`).then((response) => response.json());
+  }
+
+  static saveResult(stats, userName = DEFAULT_USER) {
+    const requestSettings = {
+      body: JSON.stringify(stats),
+      headers: {
+        'Content-Type': `application/json`
+      },
+      method: `POST`
+    };
+
+    return fetch(`${SERVER_URL}/stats/${userName}`, requestSettings);
+  }
 }
+
